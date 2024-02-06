88ae2e5475e80390411ae7bad749cea2bd150ae7
Victor <vmangeleer@uliege.be>
2024-02-06 12:26:04 +0100

    (5.1) - Adding mode to NN, classification or regression impacts outputs number of channels

--- src/debs/neural_networks.py
+++ src/debs/neural_networks.py
@@ -18,18 +18,20 @@
 # A library of neural networks to be tested as Black Sea deoxygenation emulator
 #
 # Pytorch
+import torch
 import torch.nn as nn
 
 
 class FCNN(nn.Sequential):
     r"""A fully convolutional neural network"""
 
-    def __init__(self, inputs: int, outputs: int, kernel_size: int = 3):
+    def __init__(self, inputs: int, outputs: int, problem = "classification", kernel_size: int = 3):
 
         # Initialization
         self.n_in    = inputs
-        self.n_out   = outputs
+        self.n_out   = outputs * 2 if problem == "classification" else outputs
         self.padding = kernel_size // 2
+        self.problem = problem
 
         # Architecture
         block1 = self._make_subblock(nn.Conv2d(self.n_in, 256, kernel_size, padding = self.padding))
@@ -48,7 +50,14 @@ class FCNN(nn.Sequential):
         return [conv, nn.GELU(), nn.BatchNorm2d(conv.out_channels)]
 
     def forward(self, x):
-        return super().forward(x)
+        x = super().forward(x)
+
+        if self.problem == "classification":
+            x = x.reshape(x.shape[0], self.n_out // 2, 2, x.shape[2], x.shape[3])
+            sf = nn.Softmax(dim = 2)
+            x  = sf(x)
+
+        return x
 
     def count_parameters(self,):
         print("Model parameters  =", sum(p.numel() for p in self.parameters() if p.requires_grad))
