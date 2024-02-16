(15) - Fixes - Metrics are averaged over samples not batches anymore - Now we can explore multiple time window !

src/debs/metrics.py
@@ -53,14 +53,14 @@ def RootMeanSquaredErrorPerPixel(y_pred : np.array = None, y_true : np.array = N
 class BlackSea_Metrics():
     r"""A tool to compute a large variety of metrics (scalar or visual) to assess the quality of a model."""
 
-    def __init__(self, mode: str, mask : np.array, mask_complete : np.array, treshold : float, number_of_batches : int):
+    def __init__(self, mode: str, mask : np.array, mask_complete : np.array, treshold : float, number_of_samples : int):
         r"""Initialization of the metrics computation tool"""
 
         # Storing information
         self.mask = mask
         self.mode = mode
         self.mask_complete = mask_complete
-        self.number_of_batches = number_of_batches
+        self.number_of_samples = number_of_samples
         self.treshold_normalized_oxygen = treshold
 
         # Used to store results and plots
@@ -99,7 +99,7 @@ class BlackSea_Metrics():
         batch_size, days, = y_true.shape[0], y_true.shape[1]
 
         # Stores results for each days, convert to numpy and average over number of batches (everything is summed so at the end, we will have the true average)
-        scores_temporary = np.array([self.compute_metrics_(y_pred[:, i], y_true[:, i]) for i in range(days)]) / self.number_of_batches
+        scores_temporary = np.array([self.compute_metrics_(y_pred[:, i], y_true[:, i]) for i in range(days)]) / self.number_of_samples
 
         # Adding the results to previous one or initialize it
         self.scores = np.sum([self.scores, scores_temporary], axis = 0) if isinstance(self.scores, (np.ndarray, np.generic)) else \

src/debs/neural_networks/loader.py
@@ -34,15 +34,24 @@ def load_neural_network(architecture : str, data_output : np.array, device : str
     # Extracting information
     inputs          = kwargs['Inputs']
     problem         = kwargs['Problem']
+    windows_inputs  = kwargs['Window (Inputs)']
     windows_outputs = kwargs['Window (Output)']
     architecture    = kwargs['Architecture']
     scaling         = kwargs['Scaling']
     kernel_size     = kwargs['Kernel Size']
     batch_size      = kwargs['Batch Size']
+    nb_inputs       = 0
 
     # Determining the total number of inputs
-    nb_inputs  = len(inputs)
-    nb_inputs += 1 if "mesh" in inputs else 0
+    for i in ["temperature", "salinity", "chlorophyll", "kshort", "klong"]:
+        nb_inputs += 1 if i in inputs else 0
+
+    # Adding the time dimension from the window size
+    nb_inputs *= windows_inputs
+
+    # Adding dimensions for bathymetry and mesh (additional self made inputs)
+    nb_inputs += 1 if "bathymetry" in inputs else 0
+    nb_inputs += 2 if "mesh"       in inputs else 0
 
     # Initialization of neural network and pushing it to device (GPU)
     if architecture == "FCNN":