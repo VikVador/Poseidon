c370447786a8f45c6e97f6eedc4c4b242d34aeba
Victor Mangeleer <vmangeleer@uliege.be>
2024-11-19 15:54:14 +0100

    🛠️ Config: More Readable

--- poseidon/config/__init__.py
+++ poseidon/config/__init__.py
@@ -2,22 +2,23 @@ r"""Global paths and configuration helpers."""
 
 from pathlib import Path
 
-# ----- Access to raw data
+# fmt: off
+#
+# ----- Raw Data
 #
 PROJECT = Path("/gpfs/projects/acad/bsmfc/nemo4.2.0/")
-
 PROJECT_FOLDER_DATA = PROJECT / "BSFS_BIO" / "output_HR001"
-PROJECT_FILE_MASK = PROJECT / "BSFS" / "mesh_mask.nc_new59_CMCC_noAzov"
+PROJECT_FILE_MASK   = PROJECT / "BSFS"     / "mesh_mask.nc_new59_CMCC_noAzov"
 
-# ----- Access to personnal space
+# ----- Poseidon Workspace
 #
 SCRATCH = Path("/gpfs/scratch/acad/bsmfc/victor/")
-POSEIDON = SCRATCH / "poseidon"
 
-POSEIDON_DATA = SCRATCH / "data" / "deep_learning_black_sea_3D_1995_2022.zarr"
-POSEIDON_STAT = POSEIDON / "statistics" / "statistics.zarr"
-POSEIDON_PTRC = POSEIDON / "paths" / "ptrc_T.txt"
-POSEIDON_GRID = POSEIDON / "paths" / "grid_T.txt"
-POSEIDON_MASK = POSEIDON / "mask.zarr"
-POSEIDON_MESH = POSEIDON / "mesh.zarr"
+POSEIDON       = SCRATCH  / "poseidon"
+POSEIDON_DATA  = SCRATCH  / "data"       / "deep_learning_black_sea_3D_1995_2022.zarr"
+POSEIDON_STAT  = POSEIDON / "statistics" / "statistics.zarr"
+POSEIDON_PTRC  = POSEIDON / "paths"      / "ptrc_T.txt"
+POSEIDON_GRID  = POSEIDON / "paths"      / "grid_T.txt"
+POSEIDON_MASK  = POSEIDON / "mask.zarr"
+POSEIDON_MESH  = POSEIDON / "mesh.zarr"
 POSEIDON_MODEL = POSEIDON / "models"
