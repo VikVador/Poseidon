61737b9fdee36bd129a8cda8ccc74935f0ac25cf
Victor Mangeleer <vmangeleer@uliege.be>
2024-11-20 14:42:25 +0100

    🛠️ Poseidon/Const: Reformating file better look

--- poseidon/data/const.py
+++ poseidon/data/const.py
@@ -1,12 +1,16 @@
-r"""Information about our custom datasets"""
+r"""Information about our datasets"""
 
 # fmt: off
 #
 # ----- Global Information
 #
-DATASET_TRAINING   = ("1995-01", "2015-12")
-DATASET_VALIDATION = ("2016-01", "2019-12")
-DATASET_TEST       = ("2020-01", "2022-12")
+TOY_DATASET_DATES_TRAINING   = ("2014-01-01", "2015-12-31")
+TOY_DATASET_DATES_VALIDATION = ("2019-01-01", "2019-12-31")
+TOY_DATASET_DATES_TEST       = ("2022-01-01", "2022-12-31")
+
+DATASET_DATES_TRAINING       = ("1995-01-01", "2015-12-31")
+DATASET_DATES_VALIDATION     = ("2016-01-01", "2019-12-31")
+DATASET_DATES_TEST           = ("2020-01-01", "2022-12-31")
 
 DATASET_VARIABLES_ATMOSPHERE = [
     "DOX",
@@ -23,8 +27,23 @@ DATASET_VARIABLES_SURFACE = [
 DATASET_VARIABLES = \
     DATASET_VARIABLES_ATMOSPHERE + DATASET_VARIABLES_SURFACE
 
-# ----- Preprocessing
+TOY_DATASET_REGION = {
+    "latitude":  slice(104, 232),
+    "longitude": slice(25, 281),
+    "level":     slice(0, 32), # ~144.83 [m]
+}
+
+DATASET_REGION = {
+    "latitude":  slice(0, 256),
+    "longitude": slice(0, 576),
+    "level":     slice(0, 56),
+}
+
+# ----- Preprocessing & Datasets
 #
+# In case of missing values
+DATASET_NAN_FILL = 0
+
 # Defining values domain for each variable
 DATASET_VARIABLES_CLIPPING = {
     "DOX":      (0, None),
@@ -32,26 +51,3 @@ DATASET_VARIABLES_CLIPPING = {
     "vosaline": (0, None),
     "rho":      (0, None),
 }
-
-# ----- Datasets
-#
-# In case of missing values
-DATASET_NAN_FILL = 0
-
-# Black Sea
-DATASET_REGION = {
-    "latitude":  slice(0, 256),
-    "longitude": slice(0, 576),
-    "level":     slice(0, 56),
-}
-
-# Black Sea Continental Shelf (32, 128, 256)
-TOY_DATASET_REGION = {
-    "latitude":  slice(104, 232),
-    "longitude": slice(25, 281),
-    "level":     slice(0, 32),
-}
-
-# ----- Note
-#
-# 1. Level 32 is located at ~144.83 [m]
