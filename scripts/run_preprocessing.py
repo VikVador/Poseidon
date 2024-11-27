f454ff529a23346a69f22bf259c63c509245737b
Victor Mangeleer <vmangeleer@uliege.be>
2024-11-27 14:38:16 +0100

    🛠️ Preprocessing (code and script): - Refactoring all functoins (docstrings, how they work for simplicity) - Newly updated script - Script has been tested and it works

--- scripts/run_preprocessing.py
+++ scripts/run_preprocessing.py
@@ -1,52 +1,52 @@
-r"""Scripts - Preprocess the data (standardization, fixing nans, ...)."""
+r"""Script to launch a data preprocessing pipeline."""
 
 import argparse
 
-from dawgz import Job, schedule
+from dawgz import job, schedule
+from functools import partial
 
 # isort: split
 from poseidon.config import (
-    PATH_DATA,
     PATH_STAT,
 )
 from poseidon.data.const import (
-    DATASET_TESTING_DATE_END,
-    DATASET_TRAINING_DATE_START,
+    DATASET_DATES_TEST,
+    DATASET_DATES_TRAINING,
     DATASET_VARIABLES,
     DATASET_VARIABLES_CLIPPING,
     DATASET_VARIABLES_SURFACE,
 )
-from poseidon.data.preprocessing import compute_preprocessed_dataset
+from poseidon.data.preprocessing import compute_preprocessing
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Preprocess a Black Sea dataset.")
+    parser = argparse.ArgumentParser(description="Preprocess a dataset.")
 
     parser.add_argument(
-        "--output_path",
+        "--path_output",
         "-o",
         type=str,
-        default=PATH_DATA,
+        required=True,
         help="Output .zarr file path.",
     )
     parser.add_argument(
-        "--statistics_path",
+        "--path_statistics",
         "-s",
         type=str,
         default=PATH_STAT,
-        help="Output .zarr file path.",
+        help="Pre-computed statistics .zarr file path.",
     )
     parser.add_argument(
         "--date_start",
         "-ds",
         type=str,
-        default=DATASET_TRAINING_DATE_START,
+        default=DATASET_DATES_TRAINING[0],
         help="Start date (YYYY-MM).",
     )
     parser.add_argument(
         "--date_end",
         "-de",
         type=str,
-        default=DATASET_TESTING_DATE_END,
+        default=DATASET_DATES_TEST[1],
         help="End date (YYYY-MM).",
     )
     parser.add_argument(
@@ -69,30 +69,30 @@ if __name__ == "__main__":
         type=str,
         default="slurm",
         choices=["slurm", "async"],
-        help="Computation backend.",
+        help="Computation backend, 'slurm' for cluster-based scheduling and 'async' for local execution.",
     )
 
     args = parser.parse_args()
 
-    def dawgz_preprocessing_data():
-        compute_preprocessed_dataset(
-            output_path=args.output_path,
-            statistics_path=args.statistics_path,
-            start_date=args.date_start,
-            end_date=args.date_end,
-            wandb_mode="online" if args.use_wandb else "disabled",
-            variables=args.variables,
-            variables_clipping=DATASET_VARIABLES_CLIPPING,
-            variables_surface=DATASET_VARIABLES_SURFACE,
-        )
+    dawgz_preprocessing = partial(
+        compute_preprocessing,
+        args.path_output,
+        args.path_statistics,
+        args.date_start,
+        args.date_end,
+        "online" if args.use_wandb else "disabled",
+        args.variables,
+        DATASET_VARIABLES_CLIPPING,
+        DATASET_VARIABLES_SURFACE,
+    )
 
     schedule(
-        Job(
-            dawgz_preprocessing_data,
-            cpus=16,
-            mem="256GB",
+        job(
+            dawgz_preprocessing,
+            cpus=8,
+            mem="128GB",
             name="POSEIDON-PREPROCESSING",
-            time="05-00:00:00",
+            time="02-00:00:00",
             account="bsmfc",
             partition="shared",
         ),
