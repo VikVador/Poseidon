20f8866e90b0b338a01cc8e390d834d902229516
Victor Mangeleer <vmangeleer@uliege.be>
2024-12-11 14:28:05 +0100

    ✨ Diffusion: Creation of encoding tools for neural network (used later by SIREN) and adding tests

---
+++ poseidon/network/encoding.py
@@ -0,0 +1,44 @@
+r"""Encoding blocks."""
+
+import torch
+import torch.nn as nn
+
+from torch import Tensor
+
+
+class SineEncoding(nn.Module):
+    r"""Creates a sinusoidal positional encoding.
+
+    References:
+        | Attention Is All You Need (Vaswani et al., 2017)
+        | https://arxiv.org/abs/1706.03762
+
+    Arguments:
+        features: Number of embedding features (F). Must be even.
+        omega: The maximum frequency omega.
+    """
+
+    def __init__(self, features: int, omega: float = 1e3):
+        super().__init__()
+        assert features % 2 == 0, "ERROR - The number of features must be even."
+
+        freqs = torch.linspace(0, 1, features // 2, dtype=torch.float64)
+        freqs = omega ** (-freqs)
+        self.freqs = freqs.to(dtype=torch.float32)
+
+    def forward(self, x: Tensor) -> Tensor:
+        r"""
+        Arguments:
+            x: A tensor to encode (*)
+
+        Returns:
+            Tensor: Embedded vector (*, F)
+        """
+        x = x[..., None]
+        return torch.cat(
+            (
+                torch.sin(self.freqs * x),
+                torch.cos(self.freqs * x),
+            ),
+            dim=-1,
+        )
