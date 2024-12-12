088e354c077977e37da8109c2a55dfc1b6bb0580
Victor Mangeleer <vmangeleer@uliege.be>
2024-12-12 15:42:09 +0100

    ⚙️ Diffusion.loss: - Simple refactoring and adding tests

--- poseidon/diffusion/loss.py
+++ poseidon/diffusion/loss.py
@@ -1,32 +1,35 @@
-r"""Diffusion - Custom weighted loss function."""
+r"""Diffusion training loss."""
 
 import torch
 
-# isort: split
-from poseidon.diffusion.denoiser import PoseidonDenoiser
+from torch import Tensor
 
 
 def PoseidonLoss(
-    denoiser: PoseidonDenoiser, x: torch.Tensor, sigma_t: torch.Tensor, c: torch.Tensor
-) -> torch.Tensor:
-    r"""Computes a weighted loss for the denoising model which emphasizes
-        the error differently based on the noise level.
+    x_t: Tensor,
+    x_t_denoised: Tensor,
+    sigma_t: Tensor,
+) -> Tensor:
+    r"""Weighted loss which emphasizes the error differently based on the noise level.
 
     References:
         | Elucidating the Design Space of Diffusion-Based Generative Models (Karras et al., 2022).
         | https://arxiv.org/abs/2206.00364
 
     Arguments:
-        denoiser: Denoising model to predict the clean data from noisy data.
-        x: Original input tensor with shape (B, D).
-        sigma_t: Noises scale applied to the input (B, 1).
-        c:  Tokenized time conditioning (B, 3).
+        x_t: Noisy tensor (B, D).
+        x_t_denoised: Denoised tensor (B, D).
+        sigma_t: Noises scale originally applied to input (B, 1).
 
     Returns:
-        Weighted mean squared error loss (scalar tensor).
+        Weighted mean squared error (scalar tensor).
     """
-    lambda_t = 1 / (1 + sigma_t**2)
-    x_t = x + sigma_t * torch.randn_like(x)
-    x_t_denoised = denoiser(x_t=x_t, sigma_t=sigma_t, c=c)
-    error = (x_t_denoised - x) ** 2
-    return torch.mean(lambda_t.squeeze(1) * torch.mean(error, dim=-1))
+    # fmt: off
+    #
+    # Sigma should never be zero due to exponential scaling.
+    lambda_t = (1 / sigma_t**2) + 1
+
+    se       = (x_t_denoised - x_t) ** 2
+    mse      = torch.mean(se, dim=-1)
+    wmse     = torch.mean(lambda_t * mse)
+    return wmse
