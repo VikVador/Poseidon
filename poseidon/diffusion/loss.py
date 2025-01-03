0be1717d2e35ad018ca001d5d65de319016bb292
Victor Mangeleer <vmangeleer@uliege.be>
2025-01-03 09:55:58 +0100

    ⚙️ Diffusion.loss : Cleaner loss, updated weight computation

--- poseidon/diffusion/loss.py
+++ poseidon/diffusion/loss.py
@@ -6,9 +6,9 @@ from torch import Tensor
 
 
 def PoseidonLoss(
-    x_t: Tensor,
-    x_t_denoised: Tensor,
-    sigma_t: Tensor,
+    x: Tensor,
+    x_denoised: Tensor,
+    sigma: Tensor,
 ) -> Tensor:
     r"""Weighted loss which emphasizes the error differently based on the noise level.
 
@@ -17,20 +17,19 @@ def PoseidonLoss(
         | https://arxiv.org/abs/2206.00364
 
     Arguments:
-        x_t: Noisy tensor (B, D).
-        x_t_denoised: Denoised tensor (B, D).
-        sigma_t: Noises scale originally applied to input (B, 1).
+        x: Clean tensor (B, D).
+        x_denoised: Denoised tensor (B, D).
+        sigma: Noise scale applied on x (B, 1).
 
     Returns:
-        Weighted mean squared error (scalar tensor).
+        Mean weighted mean squared error.
     """
     # fmt: off
     #
-    # Sigma should never be zero due to exponential scaling.
-    lambda_t = 1 / (1 + sigma_t**2)
+    weight = (1 / sigma**2) + 1
 
-    se       = (x_t_denoised - x_t) ** 2
+    se       = (x_denoised - x) ** 2
     mse      = torch.mean(se, dim=-1)
-    wmse     = lambda_t * mse
+    wmse     = weight * mse
     mwmse    = torch.mean(wmse)
     return mwmse
