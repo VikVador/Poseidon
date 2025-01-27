8026cb6034b638a8bb819e67f639cee0a01c285d
Victor Mangeleer <vmangeleer@uliege.be>
2025-01-27 15:07:07 +0100

    ✨ Saving Module: Adding tests !

--- tests/training/test_save.py
+++ tests/training/test_save.py
@@ -2,38 +2,29 @@ r"""Tests for the poseidon.training.save module."""
 
 import numpy as np
 import pytest
+import torch
+import torch.nn as nn
+import yaml
 
 from torch.optim import Adam
 from torch.optim.lr_scheduler import StepLR
 
 # isort: split
-from poseidon.data.const import DATASET_REGION
-from poseidon.diffusion.backbone import PoseidonBackbone
+from poseidon.training.save import (
+    save_backbone,
+    save_configuration,
+    save_tools,
+)
+
+# Generating random dimensions for testing
+INPUT_SIZE, OUTPUT_SIZE = (np.random.randint(1, 10) for _ in range(2))
 
 
+# fmt: off
 @pytest.fixture
 def fake_backbone():
-    config_unet = {
-        "kernel_size": 3,
-        "mod_features": 1,
-        "hid_channels": np.random.randint(1, 4, size=4).tolist(),
-        "hid_blocks": np.random.randint(1, 2, size=4).tolist(),
-        "dropout": None,
-        "attention_heads": {str(i): int(np.random.choice([2, 4])) for i in range(2, 4)},
-    }
-
-    config_siren = {
-        "features": np.random.randint(8, 32),
-        "n_layers": np.random.randint(1, 2),
-    }
-
-    return PoseidonBackbone(
-        dimensions=(2, 5, 3, 32, 64),
-        config_unet=config_unet,
-        config_siren=config_siren,
-        config_region=DATASET_REGION,
-    )
-
+    """A fake Backbone model for testing purposes."""
+    return nn.Linear(INPUT_SIZE, OUTPUT_SIZE)
 
 @pytest.fixture
 def temp_dir(tmp_path):
@@ -50,55 +41,90 @@ def fake_scheduler(fake_optimizer):
     return StepLR(fake_optimizer, step_size=10, gamma=0.1)
 
 
-# def test_save_backbone(temp_dir, fake_backbone):
-#     path = temp_dir
-#     name_model = "test_model"
-#     name_state = "test_state"
+def test_save_backbone(temp_dir, fake_backbone):
+    """Testing if a model is saved correctly."""
 
-#     save_backbone(path, fake_backbone, name_model, name_state)
+    path, name_model, name_state = (
+        temp_dir,
+        "test_model",
+        "test_state",
+    )
 
-#     model_folder = path / name_model / "models"
-#     assert model_folder.exists(), "Model folder was not created."
-#     assert (model_folder / f"{name_state}.pth").exists(), "Model state file was not saved."
+    save_backbone(
+        path=path,
+        model=fake_backbone,
+        name_model=name_model,
+        name_state=name_state,
+    )
 
-
-# def test_save_configuration(temp_dir):
-#     path = temp_dir
-#     name_model = "test_model"
-#     name_config = "test_config"
-#     config = {"param1": 10, "param2": 20}
-
-#     save_configuration(path, config, name_model, name_config)
-
-#     config_folder = path / name_model / "configurations"
-#     assert config_folder.exists(), "Configuration folder was not created."
-#     config_path = config_folder / f"{name_config}.yml"
-#     assert config_path.exists(), "Configuration file was not saved."
-
-#     with open(config_path, "r") as file:
-#         saved_config = yaml.safe_load(file)
-#     assert saved_config == config, "Saved configuration does not match the original."
+    model_folder = path / name_model / "models"
+    assert model_folder.exists(), "ERROR - Model folder was not created."
+    assert (
+        model_folder / f"{name_state}.pth"
+    ).exists(), "ERROR - Model state file (.pth) was not saved."
 
 
-# def test_save_tools(temp_dir, fake_optimizer, fake_scheduler):
-#     path = temp_dir
-#     name_model = "test_model"
+def test_save_configuration(temp_dir):
+    """Testing if a configuration is saved correctly."""
 
-#     save_tools(path, name_model, optimizer=fake_optimizer, scheduler=fake_scheduler)
+    path, name_model, name_config, config = (
+        temp_dir,
+        "test_model",
+        "test_config",
+        {
+            "alpha": 10,
+            "beta": 20,
+        },
+    )
 
-#     tools_folder = path / name_model / "tools"
-#     assert tools_folder.exists(), "Tools folder was not created."
+    save_configuration(
+        path=path,
+        config=config,
+        name_model=name_model,
+        name_config=name_config,
+    )
 
-#     optimizer_path = tools_folder / "optimizer.pth"
-#     assert optimizer_path.exists(), "Optimizer state file was not saved."
+    config_folder = path / name_model / "configurations"
+    config_path = config_folder / f"{name_config}.yml"
+    with open(config_path, "r") as file:
+        saved_config = yaml.safe_load(file)
 
-#     scheduler_path = tools_folder / "scheduler.pth"
-#     assert scheduler_path.exists(), "Scheduler state file was not saved."
+    assert config_folder.exists(), "ERROR - Configuration folder was not created."
+    assert config_path.exists(),   "ERROR - Configuration file was not saved."
+    assert saved_config == config, "ERROR - Saved configuration does not match the original."
 
-#     # Verify optimizer state
-#     loaded_optimizer_state = torch.load(optimizer_path)["optimizer_state_dict"]
-#     assert loaded_optimizer_state == fake_optimizer.state_dict(), "Optimizer state does not match."
 
-#     # Verify scheduler state
-#     loaded_scheduler_state = torch.load(scheduler_path)["scheduler_state_dict"]
-#     assert loaded_scheduler_state == fake_scheduler.state_dict(), "Scheduler state does not match."
+def test_save_tools(temp_dir, fake_optimizer, fake_scheduler):
+    """Testing if the optimizer and scheduler are saved correctly."""
+
+    path, name_model = (
+        temp_dir,
+        "test_model",
+    )
+
+    save_tools(
+        path=path,
+        name_model=name_model,
+        optimizer=fake_optimizer,
+        scheduler=fake_scheduler,
+    )
+
+    tools_folder = path / name_model / "tools"
+    optimizer_path = tools_folder / "optimizer.pth"
+    scheduler_path = tools_folder / "scheduler.pth"
+
+    assert tools_folder.exists(),   "ERROR - Tools folder was not created."
+    assert optimizer_path.exists(), "ERROR - Optimizer state file was not saved."
+    assert scheduler_path.exists(), "ERROR - Scheduler state file was not saved."
+
+    # Verify optimizer state
+    loaded_optimizer_state = torch.load(optimizer_path, weights_only=True)["optimizer_state_dict"]
+    assert (
+        loaded_optimizer_state == fake_optimizer.state_dict()
+    ), "ERROR - Optimizer state does not match."
+
+    # Verify scheduler state
+    loaded_scheduler_state = torch.load(scheduler_path, weights_only=True)["scheduler_state_dict"]
+    assert (
+        loaded_scheduler_state == fake_scheduler.state_dict()
+    ), "ERROR - Scheduler state does not match."
