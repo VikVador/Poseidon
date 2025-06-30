d5b8550f86dd70652dd564d094a656df460f35ae
Victor Mangeleer <vmangeleer@uliege.be>
2025-06-30 11:13:33 +0200

    ✨ New Toy and Complete Datasets

--- poseidon/data/const.py
+++ poseidon/data/const.py
@@ -14,19 +14,17 @@ VARIABLES_CLIPPING = {
 }
 
 
-# ----- Dataset: Debug
+# ----- Dataset: Black Sea Continental Shelf
 #
-TOY_DATASET_DATES_TRAINING   = ("1995-01-01", "2017-12-31")
+TOY_DATASET_DATES_TRAINING   = ("2017-01-01", "2017-12-31")
 TOY_DATASET_DATES_VALIDATION = ("2018-01-01", "2020-12-31")
 TOY_DATASET_DATES_TEST       = ("2021-01-01", "2022-12-31")
-
 
 TOY_DATASET_REGION = {
     "latitude":  slice(104, 232),
     "longitude": slice(25, 281),
-    "level": slice(0, 1),
+    "level": slice(0, 32),
 }
-
 
 TOY_DATASET_VARIABLES_ATMOSPHERE = [
     "DOX",
@@ -43,15 +41,15 @@ TOY_DATASET_VARIABLES = \
     TOY_DATASET_VARIABLES_ATMOSPHERE + TOY_DATASET_VARIABLES_SURFACE
 
 
-# ----- Dataset: Black Sea Continental Shelf
+# ----- Dataset: Black Sea
 #
-DATASET_DATES_TRAINING       = ("1995-01-01", "2017-12-31")
+DATASET_DATES_TRAINING       = ("2017-01-01", "2017-12-31")
 DATASET_DATES_VALIDATION     = ("2018-01-01", "2020-12-31")
 DATASET_DATES_TEST           = ("2021-01-01", "2022-12-31")
 
 DATASET_REGION = {
-    "latitude":  slice(104, 232),
-    "longitude": slice(25, 281),
+    "latitude":  slice(1, 257),
+    "longitude": slice(1, 577),
     "level": slice(0, 32),
 }
 
