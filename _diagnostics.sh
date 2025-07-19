6ad2147009f302ca2faf3ffa45c9665f24225e48
Victor Mangeleer <vmangeleer@uliege.be>
2025-07-19 10:11:30 +0200

    ✨ Helpers for Diagnostics

---
+++ _diagnostics.sh
@@ -0,0 +1,8 @@
+# Moving to the jobs directory
+cd scripts/diagnostics/
+
+# Launching a jupyter notebook
+python -u run_diagnostics.py -m configs/models/ ...
+
+# Moving back to the root directory
+cd ../../
