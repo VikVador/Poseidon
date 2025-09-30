c96b71060465e6104d5ac123704a61303c44c0f8
Victor Mangeleer <vmangeleer@uliege.be>
2025-09-30 09:48:49 +0200

    ✨ Adding Training Script Launcher

---
+++ _training.sh
@@ -0,0 +1,11 @@
+# Moving to the jobs directory
+cd experiments/diffusion/
+
+# Launching a jupyter notebook
+python -u run_training.py --backend slurm --config configs/debug.yml 
+
+# Moving back to the root directory
+cd ../../
+
+# Observing the queue
+watch squeue --me
