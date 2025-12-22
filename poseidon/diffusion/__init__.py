d52f1d9be347f5fc4b2785a27f9d9f74ec2cdd0d
Victor Mangeleer <vmangeleer@uliege.be>
2025-12-22 21:59:49 +0100

    ✚ Update satellite observation model parameters for chlorophyll, salinity, temperature, and sea surface height

--- poseidon/diffusion/__init__.py
+++ poseidon/diffusion/__init__.py
@@ -6,21 +6,25 @@ r"""Global constants for diffusion."""
 #
 #       y ~ N(A(x) + mu_y, sigma_y ** 2)
 #
+# Chlorophyll [mg/m³]
 SAT_CHL_MU, SAT_CHL_STD = (
-    [0.1699, 0.0],
-    [0.4049, 0.0],
+    [0.17, 0.0],
+    [0.40, 0.0],
 )
 
+# Salinity [psu]
 SAT_SAL_MU, SAT_SAL_STD = (
-    [0.0, 0.0],
-    [0.5, 0.1],
+    [-0.07, 0.0],
+    [ 0.91, 0.0],
 )
 
+# Temperature [°C]
 SAT_TEMP_MU, SAT_TEMP_STD = (
-    [-0.09, 0.13],
-    [0.69,   0.1],
+    [0.09, 0.0],
+    [0.51, 0.0],
 )
 
+# Sea Surface Height [m]
 SAT_SSH_MU, SAT_SSH_STD = (
     [0.0,         0.0],
     [1.22 * 1e-3, 0.0],
