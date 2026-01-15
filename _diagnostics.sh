46fc6b5a4403343c8e07747f658ffc55c0105205
Victor Mangeleer <vmangeleer@uliege.be>
2026-01-15 11:48:52 +0100

    The End.

--- _diagnostics.sh
+++ _diagnostics.sh
@@ -3,14 +3,7 @@ cd experiments/diagnostics/
 
 # Launching diagnostics
 #
-# Baseline
-python -u run_diagnostics.py -c configs/diagnostics-test.yml -m sweet-forest-13 -v last -cpt all -ts all -s test -g -cm
-
-# Best
-python -u run_diagnostics.py -c configs/diagnostics-test.yml -m fancy-wind-40   -v last -cpt all -ts all -s test -g -cm
- 
-# Most trained
-python -u run_diagnostics.py -c configs/diagnostics-test.yml -m scarlet-wood-42 -v last -cpt all -ts all -s test -g -cm
+python -u run_diagnostics.py -c configs/diagnostics-test.yml -m fancy-wind-40 -v last -cpt all -ts all -s test -g -cm 
 
 # Moving back to the root directory
 cd ../../
